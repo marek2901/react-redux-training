07f0f03f9b459204adebd4512c7147ef19301b44
Marek Dziewit <marek.dziewit@netguru.co>
2017-09-10 13:35:00 +0200

    Add Css for done item :tada:

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class TodoItem extends React.Component {
-    toggleDone(){
-        this.props.onToggle(this.props.item.id);
+    toggleDone() {
+        this
+            .props
+            .onToggle(this.props.item.id);
     }
-    render(){
-        return(<li key={this.props.item.id}>
-               {this.props.item.id}. {this.props.item.text}
-               <button onClick={this.toggleDone.bind(this)}>{this.props.item.done ? 'Undone' : 'Done'}</button>
-               </li>);
+    render() {
+        return (
+            <li
+                className={this.props.item.done
+                ? 'done-item'
+                : 'undone-item'}
+                key={this.props.item.id}>
+                {this.props.item.id}. {this.props.item.text}
+                <button
+                    onClick={this
+                    .toggleDone
+                    .bind(this)}>{this.props.item.done
+                        ? 'Undone'
+                        : 'Done'}</button>
+            </li>
+        );
     }
 }
 
