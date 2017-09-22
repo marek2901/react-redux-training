704fb09b2e277c9a73bce6472cdf9f2039ddaf1a
Marek Dziewit <marek2901@gmail.com>
2017-09-22 21:04:44 +0200

    Reformat resolver

--- src/components/todoItemResolver.js
+++ src/components/todoItemResolver.js
@@ -5,24 +5,24 @@ import TodoItem from './todoItem.js';
 import EditableItem from './editableTodoItem.js';
 
 class TodoItemResolver extends React.Component {
-    render() {
-    	if(this.props.item.editable){
-	        return (
-	        	  <EditableItem
-                onSubmit={this.props.onEditSubmit}
-                item={this.props.item}
-                onCancel={this.props.onEditCancel}/>
-	        );
-    	} else{
-        	return(
-              <TodoItem
-                onToggle={this.props.onToggle}
-                onToggleEdit={this.props.onToggleEdit}
-                onDelete={this.props.onDelete}
-                item={this.props.item}/>
-          );
-      }
+  render() {
+    if (this.props.item.editable) {
+      return (
+        <EditableItem
+          onSubmit={this.props.onEditSubmit}
+          item={this.props.item}
+          onCancel={this.props.onEditCancel}/>
+        );
+    } else {
+      return (
+        <TodoItem
+          onToggle={this.props.onToggle}
+          onToggleEdit={this.props.onToggleEdit}
+          onDelete={this.props.onDelete}
+          item={this.props.item}/>
+        );
     }
+  }
 }
 
 export default TodoItemResolver;
