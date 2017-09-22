5ee1b35574c130c3ef81fbc873e7948009bdf8b4
Marek Dziewit <marek2901@gmail.com>
2017-09-22 12:09:49 +0200

    Sync after action

--- src/middleware/browserStoreSync.js
+++ src/middleware/browserStoreSync.js
@@ -1,6 +1,6 @@
 const storeSync = store => next => action => {
-    localStorage.setItem('app', JSON.stringify(store.getState().app));
     next(action);
+    localStorage.setItem('app', JSON.stringify(store.getState().app));
 };
 
 export default storeSync;
